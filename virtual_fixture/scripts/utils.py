0803bc09d7a9f5d07428a83357f8b3bcb4a9d479
Hydran00 <davide.nardi-1@studenti.unitn.it>
2024-08-09 15:03:17 +0200

    projection is now done together with mesh reconstruciotn

--- virtual_fixture/scripts/utils.py
+++ virtual_fixture/scripts/utils.py
@@ -36,6 +36,7 @@ import time
 from materials import mat_sphere_transparent, mat_skin
 from tqdm import tqdm
 from ament_index_python.packages import get_package_share_directory
+
 def compute_torax_projection(mesh):
     """
     Computes the projection of the SKEL torax to the SMPL mesh
@@ -44,7 +45,7 @@ def compute_torax_projection(mesh):
     humanoid = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
     skel_path =os.path.expanduser('~')+"/SKEL_WS/SKEL/output/smpl_fit/smpl_fit_skel.obj"
     skel_model = o3d.io.read_triangle_mesh(skel_path)
-    faces_list_file_path = get_package_share_directory("virtual_fixture")+ '/skel_regions/3.txt'
+    faces_list_file_path = get_package_share_directory("virtual_fixture")+ '/skel_regions/full_torax.txt'
     projection_method = "radial" # "linear" or "radial"
     
     with open(faces_list_file_path, 'r') as file:
@@ -56,7 +57,7 @@ def compute_torax_projection(mesh):
     #     lines = file.readlines()
     #     skel_center_face_idx = [int(line.split()[4]) for line in lines]
     
-    # skel_center_vertex_id = 25736
+    skel_center_vertex_id = 25736
 
     skel_model_new  = o3d.t.geometry.TriangleMesh.from_legacy(skel_model)
     color_faces(skel_model_new, skel_faces, [1.0, 0.0, 0.0])
@@ -68,10 +69,10 @@ def compute_torax_projection(mesh):
 
     skel_face_vertices_idx = np.asarray(skel_model.triangles)[skel_faces]
     skel_vertex_positions = np.asarray(skel_model.vertices)
-    # skel_center = skel_vertex_positions[skel_center_vertex_id]
     
     # compute the mean of every vertex in the skel faces
     skel_center = np.mean([skel_vertex_positions[skel_face].mean(axis=0) for skel_face in skel_face_vertices_idx],axis=0)
+    skel_center = skel_vertex_positions[skel_center_vertex_id]
     
     
     # compute skel center taking the avg betweem min and max x and z
@@ -82,33 +83,95 @@ def compute_torax_projection(mesh):
     
     pcd = o3d.t.geometry.PointCloud()
 
+    rays = []
+    print("Preparing data for raycasting")
     for i, skel_face in enumerate(tqdm(skel_faces)):
 
-        # get xyz of the face center
-        # skel_face_vertices = skel_face_vertices_idx[i]
-        # skel_face_center = skel_vertex_positions[skel_face_vertices].mean(axis=0)
         skel_face_vertices = []
         skel_face_vertices.append(skel_vertex_positions[skel_face_vertices_idx[i][0]])
         skel_face_vertices.append(skel_vertex_positions[skel_face_vertices_idx[i][1]])
         skel_face_vertices.append(skel_vertex_positions[skel_face_vertices_idx[i][2]])
-        
 
-        # get the local z axis of the human mesh
-        # rotm = mesh.get_rotation_matrix_from_xyz((0, 0, 0))
-        # if projection_method == "linear":
-        #     rotm = mesh.get_rotation_matrix_from_xyz((0, 0, 0))
-        #     direction = -rotm[:, 2]
-        # elif projection_method == "radial":
-        for i in range(3):
+        for j in range(3):
             direction_start = skel_center
-            direction_start[1] = skel_face_vertices[i][1]
-            direction_end = skel_face_vertices[i]
+            direction_start[1] = skel_face_vertices[j][1]
+            direction_end = skel_face_vertices[j]
             direction = direction_end - direction_start
-            ray = o3d.core.Tensor([ [*skel_face_vertices[i], *direction]], dtype=o3d.core.Dtype.Float32)
-            ans = scene.cast_rays(ray)
-            smpl_faces_intersection.append(ans['primitive_ids'][0].cpu().numpy())
-            z_distance = ans['t_hit'][0].cpu().numpy()
-            smpl_points_intersection.append(skel_face_vertices[i] + z_distance * direction)
+            rays.append([*skel_face_vertices[j],*direction])
+    
+    
+    rays = o3d.core.Tensor(rays, dtype=o3d.core.Dtype.Float32)
+    
+    ans = scene.cast_rays(rays)
+    distances = ans['t_hit'].cpu().numpy()
+    # smpl_faces_intersection.append(ans['primitive_ids'][0].cpu().numpy())
+    rays = rays.cpu().numpy()
+    
+    mesh_projected = o3d.t.geometry.TriangleMesh()
+    vertices = np.zeros((len(skel_faces)*3,3))
+    faces = np.zeros((len(skel_faces),3))
+    for i, skel_face in enumerate(tqdm(skel_faces)):
+        skel_face_vertices = []
+        skel_face_vertices.append(skel_vertex_positions[skel_face_vertices_idx[i][0]])
+        skel_face_vertices.append(skel_vertex_positions[skel_face_vertices_idx[i][1]])
+        skel_face_vertices.append(skel_vertex_positions[skel_face_vertices_idx[i][2]])
+
+        z_distance1 = distances[i*3]
+        z_distance2 = distances[i*3+1]
+        z_distance3 = distances[i*3+2]
+        direction1 = rays[i*3][3:]
+        direction2 = rays[i*3+1][3:]
+        direction3 = rays[i*3+2][3:]
+        
+        if z_distance1 != np.inf and z_distance2 != np.inf and z_distance3 != np.inf:
+            new_point1 = skel_face_vertices[0] + z_distance1 * direction1
+            new_point2 = skel_face_vertices[1] + z_distance2 * direction2
+            new_point3 = skel_face_vertices[2] + z_distance3 * direction3
+            smpl_points_intersection.append(new_point1)
+            smpl_points_intersection.append(new_point2)
+            smpl_points_intersection.append(new_point3)
+            vertices[i*3][:] = new_point1
+            vertices[i*3+1][:] = new_point2
+            vertices[i*3+2][:] = new_point3
+            faces[i] = [i*3,i*3+1,i*3+2]
+
+    mesh_projected.vertex.positions = o3d.core.Tensor(vertices, dtype=o3d.core.Dtype.Float32)
+    mesh_projected.triangle.indices = o3d.core.Tensor(faces, dtype=o3d.core.Dtype.Int32)
+    
+    vertices = mesh_projected.vertex.positions.cpu().numpy()
+    triangles = mesh_projected.triangle.indices.cpu().numpy()
+    def triangle_area(v1, v2, v3):
+        return 0.5 * np.linalg.norm(np.cross(v2 - v1, v3 - v1))
+        
+    # Set the area threshold
+    area_threshold = 0.0005
+
+    # Find the triangles that have an area below the threshold
+    valid_triangles = []
+    valid_vertices = []
+    for tri in triangles:
+        v1, v2, v3 = vertices[tri]
+        area = triangle_area(v1, v2, v3)
+        if area <= area_threshold:
+            # ensure that edges are smaller than 0.02 meter
+            if (np.linalg.norm(v1-v2) < 0.25) and (np.linalg.norm(v2-v3) < 0.02) and (np.linalg.norm(v1-v3) < 0.02):
+                valid_triangles.append(tri)
+                valid_vertices.extend([v1, v2, v3])
+         
+    # Create a new mesh with the filtered triangles
+    valid_triangles = np.array(valid_triangles)
+    filtered_mesh = o3d.geometry.TriangleMesh()
+    filtered_mesh = o3d.t.geometry.TriangleMesh.from_legacy(filtered_mesh)
+    filtered_mesh.vertex.positions = o3d.core.Tensor(valid_vertices, dtype=o3d.core.Dtype.Float32)
+    filtered_mesh.triangle.indices = o3d.core.Tensor(valid_triangles, dtype=o3d.core.Dtype.Int32)
+    filtered_mesh.compute_vertex_normals()
+    # # filtered_mesh.paint_uniform_color([1, 0, 0])
+    triangle_colors = np.ones((len(filtered_mesh.triangle.indices), 3))
+    # triangle_colors[::3] = [1, 0, 0]
+    for i in range(0,len(triangle_colors)):
+        triangle_colors[i] = [1, 0, 0]
+    filtered_mesh.triangle.colors = o3d.core.Tensor(triangle_colors, dtype=o3d.core.float32)
+    o3d.visualization.draw([filtered_mesh])
         # else:
         #     raise ValueError("Invalid projection method")
 
@@ -123,6 +186,18 @@ def compute_torax_projection(mesh):
     pcd.point.colors[:, 0] = 1.0
     # pcd.point.positions[:, 2] -= 0.1
 
+
+    # compute the distance between the skel and the skin
+    # distance_skel_skin = 0.02
+    
+    # # scale the skel mesh by the distance   
+    # # Compute the bounding box to get an idea of the size of the mesh
+    # bbox = skel_model.get_axis_aligned_bounding_box()
+    # bbox_size = np.max(bbox.get_extent())  # Maximum dimension of the bounding box
+    # scaling_factor = 1 + (distance_skel_skin / bbox_size) + 0.1
+    # skel_model.scale(scaling_factor,center=skel_model.get_center())
+
+
     geometries = [
     {
         "name": "humanoid",
@@ -131,21 +206,21 @@ def compute_torax_projection(mesh):
     },
     {
         "name": "skel",
-        "geometry": skel_model,
+        "geometry": filtered_mesh,
         # "material": mat_sphere_transparent
     },
-    {
-        "name": "pcd",
-        "geometry": pcd,
-        # "material": mat_sphere_transparent
-    },
+    # {
+    #     "name": "pcd",
+    #     "geometry": pcd,
+    #     # "material": mat_sphere_transparent
+    # },
     {
         "name": "mesh center",
         "geometry": o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2, origin=skel_center),
     }
     ]
 
-    color_faces(humanoid, smpl_faces_intersection, [1.0, 0.0, 0.0])
+    # color_faces(humanoid, smpl_faces_intersection, [1.0, 0.0, 0.0])
     o3d.visualization.draw([humanoid])
     
     humanoid.compute_vertex_normals()
