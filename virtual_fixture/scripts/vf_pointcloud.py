6feedbe57c480e73d802d3b394d404b9db4a9547
Davide <davide.nardi-1@studenti.unitn.it>
2024-08-14 14:15:53 +0200

    added raycast logic for vf

--- virtual_fixture/scripts/vf_pointcloud.py
+++ virtual_fixture/scripts/vf_pointcloud.py
@@ -47,16 +47,29 @@ class VirtualFixtureDemo(Node):
         
         # Initialize sphere properties
         sphere_radius = POINT_RADIUS
-        self.sphere_center = self.surface.get_center() + (0.05, 0.1, 0.16)
-        self.sphere_center = self._enforce_virtual_fixture(self.sphere_center, self.surface, sphere_radius)
+        self.sphere_center = [0.05,0.501,0.25226753]#self.surface.get_center() + (0.05, 0.1, 0.16)
+        self.sphere_target_center = [0.05,0.501,0.25226753]
+        self.old_sphere_center = self.sphere_center
+        self.old_sphere_target_center = self.sphere_target_center
+        # self.sphere_center = self._enforce_virtual_fixture(self.sphere_center, self.surface, sphere_radius)
 
         self.sphere = o3d.geometry.TriangleMesh.create_sphere(radius=sphere_radius)
         self.sphere.translate(self.sphere_center)
-        self.sphere.paint_uniform_color([0.1, 0.1, 0.7])
+        self.sphere.paint_uniform_color([0, 1, 0])
         self.viz.add_geometry(self.sphere)
+
+        self.sphere_target = o3d.geometry.TriangleMesh.create_sphere(radius=sphere_radius)
+        self.sphere_target.translate(self.sphere_target_center)
+        self.sphere_target.paint_uniform_color([1, 0, 0])
+        self.viz.add_geometry(self.sphere_target)
 
         self.reference_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1)
         self.viz.add_geometry(self.reference_frame)
+        self.ray_line = o3d.geometry.LineSet()
+        self.ray_line.points = o3d.utility.Vector3dVector([[0, 0, 0], [0, 0, 0]])
+        self.ray_line.lines = o3d.utility.Vector2iVector([[0, 1]])
+        self.ray_line.colors = o3d.utility.Vector3dVector(np.array([[1, 0, 0]]))
+        self.viz.add_geometry(self.ray_line)
 
         # Initialize camera parameters
         self.view_point_filename = "view_point.json"
@@ -72,19 +85,70 @@ class VirtualFixtureDemo(Node):
         self._run_main_loop()
 
     def _enforce_virtual_fixture(self, sphere_center, surface, sphere_radius):
-        distance, closest_point = self._point_to_mesh_distance(sphere_center, surface)
+        # distance, closest_point = self._point_to_mesh_distance(sphere_center, surface)
         
-        if distance < sphere_radius:
-            direction = sphere_center - closest_point
-            direction /= np.linalg.norm(direction)
-            sphere_center = closest_point + direction * sphere_radius
+        # if distance < sphere_radius:
+        #     direction = sphere_center - closest_point
+        #     direction /= np.linalg.norm(direction)
+        #     sphere_center = closest_point + direction * sphere_radius
 
         # if not self._is_sphere_outside(sphere_center, surface, sphere_radius):
         #     direction = sphere_center - closest_point
         #     direction /= np.linalg.norm(direction)
         #     sphere_center = closest_point + direction * sphere_radius
 
+
+        
+        sphere_center = self.enforce(self.sphere_target_center, self.old_sphere_center, sphere_radius)
+
+
         return sphere_center
+
+
+    def enforce(self,sphere_center,old_sphere_center, sphere_radius):
+        self.get_logger().info("Sphere center: " + str(sphere_center))
+        ray_direction = sphere_center - old_sphere_center
+        ray_direction_normalized = ray_direction / np.linalg.norm(ray_direction)
+        ray_start = old_sphere_center
+
+        rays = o3d.core.Tensor([ [*ray_start, *ray_direction_normalized]], dtype=o3d.core.Dtype.Float32)
+        intersections = self.scene.cast_rays(rays)
+
+        self.ray_line.points = o3d.utility.Vector3dVector([ray_start, ray_start + ray_direction_normalized*0.1])
+        self.viz.update_geometry(self.ray_line)
+        # check if the ray intersects with the surface
+        # no intersection
+        self.get_logger().info(f"t_hit: {intersections['t_hit'].cpu().numpy()}")
+        if intersections['t_hit'][0].cpu().numpy() == np.inf:
+            self.get_logger().info("No intersection")
+            return sphere_center
+
+        # intersection occurs after the target point
+
+        if np.linalg.norm(ray_direction) + sphere_radius < intersections['t_hit'][0].cpu().numpy():
+            self.get_logger().info("A")
+            distance, closest_point = self._point_to_mesh_distance(sphere_center, self.surface)
+            if distance < sphere_radius:
+                direction = sphere_center - closest_point
+                direction /= np.linalg.norm(direction)
+                sphere_center = closest_point + direction * sphere_radius
+            
+            return sphere_center
+        
+        # intersection occurs before the target point -> enforce VF
+        else:
+            self.get_logger().info("B")
+            # return intersections['t_hit'][0].cpu().numpy()*ray_direction_normalized + ray_start - sphere_radius*ray_direction_normalized
+
+            # compute closest point on the surface
+            distance, closest_point = self._point_to_mesh_distance(sphere_center, self.surface)
+            if distance < sphere_radius:
+                direction = sphere_center - closest_point
+                direction /= np.linalg.norm(direction)
+                sphere_center = closest_point + direction * sphere_radius
+            return sphere_center
+
+
 
     def _is_sphere_outside(self, sphere_center, surface, sphere_radius):
         distance, _ = self._point_to_mesh_distance(sphere_center, surface)
@@ -109,43 +173,45 @@ class VirtualFixtureDemo(Node):
                 self._handle_key_events(event)
             
             self.sphere_center = self._move_sphere(self.sphere_center, self.direction_vector, MOVEMENT_SPEED)
+            
             self.sphere_center = self._enforce_virtual_fixture(self.sphere_center, self.surface, POINT_RADIUS)
             self.sphere.translate(self.sphere_center, relative=False)
             self.viz.update_geometry(self.sphere)
             
             self.viz.poll_events()
             self.viz.update_renderer()
-            time.sleep(0.01)
+            time.sleep(0.03)
+            self.old_sphere_center = self.sphere_center
 
     def _handle_key_events(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                self.direction_vector[2] += 1.0
-            elif event.key == pygame.K_s:
                 self.direction_vector[2] -= 1.0
-            elif event.key == pygame.K_a:
-                self.direction_vector[0] -= 1.0
-            elif event.key == pygame.K_d:
+            if event.key == pygame.K_s:
+                self.direction_vector[2] += 1.0
+            if event.key == pygame.K_a:
                 self.direction_vector[0] += 1.0
-            elif event.key == pygame.K_q:
+            if event.key == pygame.K_d:
+                self.direction_vector[0] -= 1.0
+            if event.key == pygame.K_q:
                 self.direction_vector[1] -= 1.0
-            elif event.key == pygame.K_e:
+            if event.key == pygame.K_e:
                 self.direction_vector[1] += 1.0
-            elif event.key == pygame.K_v:
+            if event.key == pygame.K_v:
                 self.save_view_point(self.view_point_filename)
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
-                self.direction_vector[2] -= 1.0
-            elif event.key == pygame.K_s:
                 self.direction_vector[2] += 1.0
-            elif event.key == pygame.K_a:
-                self.direction_vector[0] += 1.0
-            elif event.key == pygame.K_d:
+            if event.key == pygame.K_s:
+                self.direction_vector[2] -= 1.0
+            if event.key == pygame.K_a:
                 self.direction_vector[0] -= 1.0
-            elif event.key == pygame.K_q:
+            if event.key == pygame.K_d:
+                self.direction_vector[0] += 1.0
+            if event.key == pygame.K_q:
                 self.direction_vector[1] += 1.0
-            elif event.key == pygame.K_e:
+            if event.key == pygame.K_e:
                 self.direction_vector[1] -= 1.0
 
     def save_view_point(self, filename):
