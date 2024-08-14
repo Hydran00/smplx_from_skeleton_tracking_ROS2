79a0d9b835db73ad00f683832bdac2f1a9527401
Davide <davide.nardi-1@studenti.unitn.it>
2024-08-14 17:11:20 +0200

    Minor

--- virtual_fixture/scripts/vf_pointcloud.py
+++ virtual_fixture/scripts/vf_pointcloud.py
@@ -33,8 +33,13 @@ class VirtualFixtureDemo(Node):
         pygame.display.set_mode((screen_width, screen_height))        
         
         # Load and prepare the surface
-        load_path = os.path.expanduser('~') + "/SKEL_WS/ros2_ws/projected_skel.ply"
-        self.surface = o3d.io.read_triangle_mesh(load_path)
+        # load_path = os.path.expanduser('~') + "/SKEL_WS/ros2_ws/projected_skel.ply"
+        # self.surface = o3d.io.read_triangle_mesh(load_path)
+
+        # load stanford bunny from open3d examples
+        dataset = o3d.data.BunnyMesh()
+        self.surface = o3d.io.read_triangle_mesh(dataset.path)
+
         self.viz.add_geometry(self.surface)
         self.surface.translate((0, 0.5, 0), relative=False)
         self.viz.add_geometry(self.surface)
@@ -105,57 +110,38 @@ class VirtualFixtureDemo(Node):
         return sphere_center
 
 
-    def enforce(self,sphere_center,old_sphere_center, sphere_radius):
-        self.get_logger().info("Sphere center: " + str(sphere_center))
-        ray_direction = sphere_center - old_sphere_center
-        ray_direction_normalized = ray_direction / np.linalg.norm(ray_direction)
-        ray_start = old_sphere_center
+    def enforce(self, sphere_target_center,old_sphere_center, sphere_radius):
 
-        rays = o3d.core.Tensor([ [*ray_start, *ray_direction_normalized]], dtype=o3d.core.Dtype.Float32)
-        intersections = self.scene.cast_rays(rays)
-
-        self.ray_line.points = o3d.utility.Vector3dVector([ray_start, ray_start + ray_direction_normalized*0.1])
-        self.viz.update_geometry(self.ray_line)
-        # check if the ray intersects with the surface
-        # no intersection
-        self.get_logger().info(f"t_hit: {intersections['t_hit'].cpu().numpy()}")
-        if intersections['t_hit'][0].cpu().numpy() == np.inf:
-            self.get_logger().info("No intersection")
-            return sphere_center
-
-        # intersection occurs after the target point
-
-        if np.linalg.norm(ray_direction) + sphere_radius < intersections['t_hit'][0].cpu().numpy():
-            self.get_logger().info("A")
-            distance, closest_point = self._point_to_mesh_distance(sphere_center, self.surface)
-            if distance < sphere_radius:
-                direction = sphere_center - closest_point
-                direction /= np.linalg.norm(direction)
-                sphere_center = closest_point + direction * sphere_radius
-            
-            return sphere_center
+        if np.linalg.norm(sphere_target_center - old_sphere_center) > 0.0001:
+            ray_start = old_sphere_center
+            ray_end = sphere_target_center
+            ray_direction_normalized = ray_end - ray_start / np.linalg.norm(ray_end - ray_start)
+            self.ray_line.points = o3d.utility.Vector3dVector([ray_start, ray_end])
+            self.viz.update_geometry(self.ray_line)
         
-        # intersection occurs before the target point -> enforce VF
+        distance, closest_point = self._point_to_mesh_distance(old_sphere_center, self.surface)
+        if distance - 0.005 < sphere_radius:
+            direction = old_sphere_center - closest_point
+            direction /= np.linalg.norm(direction)
+            # sphere center is casted to the surface
+            # vf_center = closest_point + direction * sphere_radius
+
+            # nullify the component of the target direction parallel to direction
+            target_direction = sphere_target_center - old_sphere_center
+            target_direction = target_direction - np.dot(target_direction, direction) * direction
+            target_direction /= np.linalg.norm(target_direction)
+            vf_center = closest_point + target_direction * sphere_radius 
+            return vf_center
         else:
-            self.get_logger().info("B")
-            # return intersections['t_hit'][0].cpu().numpy()*ray_direction_normalized + ray_start - sphere_radius*ray_direction_normalized
-
-            # compute closest point on the surface
-            distance, closest_point = self._point_to_mesh_distance(sphere_center, self.surface)
-            if distance < sphere_radius:
-                direction = sphere_center - closest_point
-                direction /= np.linalg.norm(direction)
-                sphere_center = closest_point + direction * sphere_radius
-            return sphere_center
-
+            return sphere_target_center
 
 
     def _is_sphere_outside(self, sphere_center, surface, sphere_radius):
         distance, _ = self._point_to_mesh_distance(sphere_center, surface)
         return distance >= sphere_radius
 
-    def _move_sphere(self, sphere_center, direction_vector, speed):
-        return sphere_center + direction_vector * speed
+    def _move_sphere(self, current, direction_vector, speed):
+        return current + direction_vector * speed
 
     def _point_to_mesh_distance(self, point, mesh):
         query_point = o3d.core.Tensor([point], dtype=o3d.core.Dtype.Float32)
@@ -172,16 +158,19 @@ class VirtualFixtureDemo(Node):
                     running = False
                 self._handle_key_events(event)
             
-            self.sphere_center = self._move_sphere(self.sphere_center, self.direction_vector, MOVEMENT_SPEED)
+            self.sphere_target_center = self._move_sphere(self.old_sphere_target_center, self.direction_vector, MOVEMENT_SPEED)
             
-            self.sphere_center = self._enforce_virtual_fixture(self.sphere_center, self.surface, POINT_RADIUS)
+            self.sphere_target.translate(self.sphere_target_center, relative=False)
+            self.viz.update_geometry(self.sphere_target)
+            self.vf = self._enforce_virtual_fixture(self.sphere_target_center, self.surface, POINT_RADIUS)
+            self.sphere_center = self.old_sphere_center + (self.vf - self.old_sphere_center) * 0.1
             self.sphere.translate(self.sphere_center, relative=False)
             self.viz.update_geometry(self.sphere)
-            
             self.viz.poll_events()
             self.viz.update_renderer()
             time.sleep(0.03)
             self.old_sphere_center = self.sphere_center
+            self.old_sphere_target_center = self.sphere_target_center
 
     def _handle_key_events(self, event):
         if event.type == pygame.KEYDOWN:
